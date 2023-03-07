33b1b28938902fd96d1c057ba75ccdc601474eb1
Carlo Tasca <c.tasca.1971@gmail.com>
2023-03-07 07:46:11 +0100

    Initialise module

---
+++ Ctasca/WmsSync/registration.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    "Ctasca_WmsSync",
+    __DIR__
+);
